b3dfe288971ac80cca01196d0e69e91218be836c
Laurentiu-Laslau <laurentiu-leonard.laslau@student.tuiasi.ro>
2026-02-27 09:14:10 +0200

    updateee

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 
 class main {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD!");   
+        System.out.println("HELLO WORLD!");
+
+        System.out.println("UPDAAATEEE");
     }
 }
